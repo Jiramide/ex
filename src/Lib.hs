a564c813fa457913110c013d2809b92408f866f1
Jade Piramide <jadepiramidepogi@gmail.com>
2023-02-07 17:49:25 -0800

    Start creating the application state and the public interface.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,41 @@
 module Lib
-  (
+  ( FileSystemView
+  , FileSystemCursor
+  , fileSystemDown
+  , fileSystemUp
+  , fileSystemFocusParent
+  , fileSystemFocusChild
   ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import System.IO
+
+data FileSystemView
+  = DirectoryView FilePath
+  | FileView FilePath
+  deriving (Show, Eq, Ord)
+
+data FileSystemCursor = 
+  FileSystemCursor
+    { path :: FilePath
+    , parent :: Maybe FileSystemView
+    , child :: Maybe FileSystemView
+    , cursorPrev :: [FilePath]
+    , cursorNext :: [FilePath]
+    }
+  deriving (Show, Eq, Ord)
+
+fileSystemDown :: FileSystemCursor -> Maybe FileSystemCursor
+fileSystemDown = undefined
+
+fileSystemUp :: FileSystemCursor -> Maybe FileSystemCursor
+fileSystemUp = undefined
+
+fileSystemFocusParent :: FileSystemCursor -> Maybe FileSystemCursor
+fileSystemFocusParent = undefined
+
+fileSystemFocusChild :: FileSystemCursor -> Maybe FileSystemCursor
+fileSystemFocusChild = undefined
+
+cursorFromFilePath :: FilePath -> IO FileSystemCursor
+cursorFromFilePath = do
+  return undefined
